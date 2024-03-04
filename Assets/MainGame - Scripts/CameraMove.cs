d7c68ab60bb1c09b54f9309003aa39b03b6a5548
Kro <124914389+Krowno04@users.noreply.github.com>
2024-03-04 16:28:22 -0500

    Code cleanup

--- Assets/MainGame - Scripts/CameraMove.cs
+++ Assets/MainGame - Scripts/CameraMove.cs
@@ -8,17 +8,14 @@ public class CameraMove : MonoBehaviour
     public Vector3 camOffset;
     public float camMoveTime;
     public Vector3 camVelocity = Vector3.zero;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
+        //The function to create the movement/following of the camTarget object
         Vector3 camTransform = new Vector3 (camTarget.position.x + camOffset.x, camTarget.position.y + camOffset.y, camOffset.z);
         //SmoothDamp function found via https://www.youtube.com/watch?v=ZBj3LBA2vUY
+        //Uses camTarget, camOffset, camMoveTime, and camVelocity to have the camera follow the camTarget with a small bit of move time delay to smooth it.
         transform.position = Vector3.SmoothDamp(transform.position, camTransform, ref camVelocity, camMoveTime);
     }
 }
