a577355cf6869f50d46233ce754ff9a33fd5ae39
Kro <124914389+Krowno04@users.noreply.github.com>
2024-03-06 18:00:53 -0500

    HPSystem and danger system
    HP System branch is breaking for me, so working here now

--- Assets/MainGame - Scripts/HPSystem.cs
+++ Assets/MainGame - Scripts/HPSystem.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HPSystem : MonoBehaviour
 {
+    public TextMeshProUGUI HPText = null;
+    public int HPCount = 0;
+    public PlayerScript PlayerScript;
     // Start is called before the first frame update
     void Start()
     {
-        
+        HPText.text = "HP: " + HPCount;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "Danger")
+        {
+            HPCount--;
+            HPText.text = "HP: " + HPCount;
+            if (HPCount <= 0)
+            {
+
+                StartCoroutine(BackToBuild());
+            }
+        }
+    }
+
+    IEnumerator BackToBuild()
+    {
+        PlayerScript.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        HPCount = 0;
+        HPText.text = "YOU DIED";
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("WeaponBuildingUI");
     }
 }
