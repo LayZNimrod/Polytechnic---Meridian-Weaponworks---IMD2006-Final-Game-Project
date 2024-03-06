f836c199e2be8bf5fb21c02db37f2bf758155e68
Kro <124914389+Krowno04@users.noreply.github.com>
2024-03-06 16:51:11 -0500

    UI Sprites added
    Added the element logo sprites and weapon sprites to the Weapon Build UI

---
+++ Assets/Weapon Building Menu - Scripts/WeapSpriteSwicher.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeapSpriteSwicher : MonoBehaviour
+{
+    public Sprite WrenchSprite;
+    public Sprite CannonSprite;
+    //public Sprite SpearSprite;
+
+    public MenuScript script;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (script.chosenWeapon)
+        {
+            case MenuScript.Weapon.Wrench:
+                {
+                    this.gameObject.GetComponent<SpriteRenderer>().sprite = WrenchSprite;
+                    break;
+                }
+            case MenuScript.Weapon.Cannon:
+                {
+                    this.gameObject.GetComponent<SpriteRenderer>().sprite = CannonSprite;
+                    break;
+                }
+        }
+    }
+}
