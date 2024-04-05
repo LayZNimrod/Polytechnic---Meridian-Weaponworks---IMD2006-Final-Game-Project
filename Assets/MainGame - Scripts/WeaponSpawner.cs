c8d976808995c33ab1fbbb9cfa2458753403cd34
LayZNimrod <2zachmiller@gmail.com>
2024-04-05 10:04:11 -0400

    weapon not work on first build load fix
    I fixed the error where the weapon not work on first build load. it was because the weaponScript was trying to access player.instance when it did not exist. fixed by using a coroutine

--- Assets/MainGame - Scripts/WeaponSpawner.cs
+++ Assets/MainGame - Scripts/WeaponSpawner.cs
@@ -10,7 +10,7 @@ public class WeaponSpawner : MonoBehaviour
     public GameObject spear;
     public StatHandler stat;
 
-    // Start is called before the first frame update
+        
     private void OnEnable()
     {
         switch (ChoiceCarrier.ChosenWeapon)
