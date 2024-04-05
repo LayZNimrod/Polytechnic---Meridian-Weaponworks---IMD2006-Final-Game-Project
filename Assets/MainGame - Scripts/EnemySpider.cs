4badd066414c9340cabbcf0d60985b07c013e2ae
Kro <124914389+Krowno04@users.noreply.github.com>
2024-04-05 19:02:27 -0400

    EnemyTells System set up! also anim for spider!

--- Assets/MainGame - Scripts/EnemySpider.cs
+++ Assets/MainGame - Scripts/EnemySpider.cs
@@ -10,10 +10,15 @@ public class EnemySpider : MonoBehaviour
 {
     public GameObject spider;
     public GameObject SpiderProj;
+    public GameObject EnemyTell1;
+    public GameObject EnemyTell2;
+    public GameObject EnemyTell3;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(ProjFire());
+        //EnemyTell.GetComponent<SpriteRenderer>().enabled = false;
         spider = this.gameObject;
     }
 
@@ -21,9 +26,18 @@ public class EnemySpider : MonoBehaviour
     {
         while (true) 
         {
-            yield return new WaitForSeconds(5);
+            yield return new WaitForSeconds(2);
+            EnemyTell1.GetComponent<SpriteRenderer>().enabled = true;
+            yield return new WaitForSeconds(1);
+            EnemyTell2.GetComponent<SpriteRenderer>().enabled = true;
+            yield return new WaitForSeconds(1);
+            EnemyTell3.GetComponent<SpriteRenderer>().enabled = true;
+            yield return new WaitForSeconds(1);
             Vector3 EnemyPos = new Vector3(transform.position.x, transform.position.y, 0f);
             Instantiate(SpiderProj, EnemyPos, Quaternion.identity);
+            EnemyTell1.GetComponent<SpriteRenderer>().enabled = false;
+            EnemyTell2.GetComponent<SpriteRenderer>().enabled = false;
+            EnemyTell3.GetComponent<SpriteRenderer>().enabled = false;
         }
     }
 }
